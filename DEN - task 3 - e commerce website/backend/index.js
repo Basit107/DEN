f1fb01efa649cb91c8149acad10bd68cb007d053
Basit107 <shaikhbasit1657@gmail.com>
2024-09-30 00:17:59 +0500

    Created API in Index.js file

--- DEN - task 3 - e commerce website/backend/index.js
+++ DEN - task 3 - e commerce website/backend/index.js
@@ -7,6 +7,7 @@ const jwt = require("jsonwebtoken")
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+const { type } = require("os");
 
 app.use(express.json());
 app.use(cors());
@@ -31,23 +32,115 @@ app.listen(port, (error) => {
 
 })
 
+// Image Storage engine
+const storage = multer.diskStorage({
+  destination: './upload/images',
+  filename:(request, file, cb) => {
+    return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+  }
+})
+
+const upload = multer({storage:storage})
 
 
+//  Creatintg Upload Endpoint for Images.
+app.use('/images', express.static('upload/images'))
+
+app.post("/upload", upload.single('product'), (request, response) => {
+  response.json({
+    success:1,
+    image_url:`http://localhost:${port}/images/${request.file.filename}`
+  })
+})
+
+// Schema For Creating Products
+
+const Product = mongoose.model("product", {
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number,
+    required: true,
+  },
+  old_price: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+
+})
+
+app.post('/addproduct', async (request, response) => {
+  let products = await Product.find({});
+  let id;
+  if (products.length > 0) {
+    let last_product_array = products.slice(-1);
+    let last_product = last_product_array[0];
+    id = last_product.id + 1;
+  }
+  else {
+    id = 1;
+  }
+
+  const product = new Product({
+    id:id,
+    name:request.body.name,
+    image:request.body.image,
+    category:request.body.category,
+    new_price:request.body.new_price,
+    old_price:request.body.old_price,
+    available:request.body.available
+  });
+
+  console.log(product);
+  await product.save();
+  console.log("Product is Saved");
+  response.json({
+    success: true,
+    name: request.body.name,
+  })
+})
+
+// Creating API for Deletiong Products
+app.post('/removeproduct', async (request, response) => {
+  await Product.findOneAndDelete({id:request.body.id});
+  console.log("Product is Removed");
+  response.json({
+    success: true,
+    name: request.body.name,
+  })
+  
+})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Creqating API For Getting All Products.
+app.get('/allproducts', async (request, response) => {
+  let products = await Product.find({});
+  console.log("All products Fetched.");
+  response.send(products);
+  
+})
 
 
 
