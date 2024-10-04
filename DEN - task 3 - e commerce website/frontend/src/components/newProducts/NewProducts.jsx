5bf2d68fd1255c74bfe238ad38a5ddee92b1a7a2
Basit107 <shaikhbasit1657@gmail.com>
2024-10-04 23:29:16 +0500

    securing connection To Database

--- DEN - task 3 - e commerce website/frontend/src/components/newProducts/NewProducts.jsx
+++ DEN - task 3 - e commerce website/frontend/src/components/newProducts/NewProducts.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './newProducts.css'
-import new_collections from "../assets/new_collections";
+// import new_collections from "../assets/new_collections";
 import Item from "../item/Item";
 
 
 const NewProducts = () => {
+
+    const [new_products, setNewProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:4000/newproducts')
+        .then((response) => response.json()).then((data) => setNewProducts(data));
+    }, [])
+
     return (
         <div className="new-products">
             <h1>NEW PRODUCTS</h1>
             <hr />
             <div className="products">
-                {new_collections.map((item, i)=> {
+                {new_products.map((item, i)=> {
                     return <Item key={i} id={item.id} name={item.name}  image={item.image} new_price={item.new_price} old_price={item.old_price} />
                 })}
             </div>
